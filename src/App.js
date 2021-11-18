63f14d685e8d422eb4a0c1b3e9b535bc809683b6
Sander Vanstaen <vanstaensander@gmail.com>
2021-11-18 15:48:24 +0100

    renamed in file as well

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/Header/header.component';
+import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
